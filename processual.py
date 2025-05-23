f8523ea95fb0ada64893bbe1d51b14397d6d92b1
adrian-oliveir <adrian.work6n@gmail.com>
2025-05-23 10:57:27 -0300

    Create processual.py
    esse é o código que eu criei para o trabalho processual baseado em um jogo

---
+++ processual.py
@@ -0,0 +1,14 @@
+nickname = input ("digite seu nick name, por favor: ")
+senha = input ("digite sua senha aqui: ")
+email = input ("digite seu e-mail aqui: ")
+
+
+print(f"\nok, {email}! email concluido com sucesso.")
+print(f"confirmado sua senha {senha}!.")
+print(f"\folá, {nickname} seja bem vindo ao nosso jogo!")
+
+servidor = input ("selecione o servidor: ")
+mundo = input ("selecione mundo: ")
+
+print(f"servidor:{servidor} selcionado com sucesso.")
+print(f" bem vindo ao mundo {mundo} se divirta.")
