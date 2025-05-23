c1bd43529c77be32ec8eecefd93818e939081c74
adrian-oliveir <adrian.work6n@gmail.com>
2025-05-23 11:30:49 -0300

    Create codigo2.py

---
+++ codigo2.py
@@ -0,0 +1,10 @@
+contador = 0
+data = 28
+
+while contador <= 100:
+    mult= contador * data
+    if mult >= 1000: 
+      print ("Maior ou igual a mil")
+      
+    contador += 1
+    print (mult)
