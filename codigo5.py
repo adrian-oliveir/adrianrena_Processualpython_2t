3064779ca0c1b3d51af4d01cc033fad109af57d5
adrian-oliveir <adrian.work6n@gmail.com>
2025-06-02 08:33:17 -0300

    Update codigo5.py

--- codigo5.py
+++ codigo5.py
@@ -1,1 +1,17 @@
+nomes = []
+contador = 0
 
+while contador <= 10:
+    nome = input("Digite o nome (ou digite 'fim' para encerrar): ")
+
+    if nome.lower() == 'fim':
+        break
+
+    if nome.isalpha():
+        nomes.append(nome)
+        contador += 1
+    else:
+        print("Nome inválido. Por favor, digite apenas letras.")
+
+nomes.sort() 
+print(nomes)
