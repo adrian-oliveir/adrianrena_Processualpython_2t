1fc84c6a4c89613dca8a6d28d72c41dbe43c73e9
adrian-oliveir <adrian.work6n@gmail.com>
2025-06-18 09:50:59 -0300

    prova.py

---
+++ prova.py
@@ -0,0 +1,127 @@
+chamada = ["--1Adrian--", "Angelo-", "Arthur", "25 Raphael"]
+# Define a largura padrão baseada no nome mais longo
+largura = max(len(nome) for nome in chamada) + 4
+
+# Linha superior
+linha_topo = "".join("+" + "-" * largura for _ in chamada) + "+"
+# Linha do meio com os nomes centralizados
+linha_meio = "".join("|" + nome.center(largura) for nome in chamada) + "|"
+# Linha inferior
+linha_base = "".join("+" + "-" * largura for _ in chamada) + "+"
+
+# Exibe o quadro
+print(linha_topo)
+print(linha_meio)
+print(linha_base)
+
+chamada = ["Christopher", "Douglas", "Emanuélly", "Everson"]
+# Define a largura padrão baseada no nome mais longo
+largura = max(len(nome) for nome in chamada) + 4
+
+# Linha superior
+linha_topo = "".join("+" + "-" * largura for _ in chamada) + "+"
+# Linha do meio com os nomes centralizados
+linha_meio = "".join("|" + nome.center(largura) for nome in chamada) + "|"
+# Linha inferior
+linha_base = "".join("+" + "-" * largura for _ in chamada) + "+"
+
+# Exibe o quadro
+print(linha_topo)
+print(linha_meio)
+print(linha_base)
+
+chamada = ["--Gabriel--", "Guilherme","Gustavo", "Gustavo"]
+# Define a largura padrão baseada no nome mais longo
+largura = max(len(nome) for nome in chamada) + 4
+
+# Linha superior
+linha_topo = "".join("+" + "-" * largura for _ in chamada) + "+"
+# Linha do meio com os nomes centralizados
+linha_meio = "".join("|" + nome.center(largura) for nome in chamada) + "|"
+# Linha inferior
+linha_base = "".join("+" + "-" * largura for _ in chamada) + "+"
+
+# Exibe o quadro
+print(linha_topo)
+print(linha_meio)
+print(linha_base)
+
+chamada = ["---João----", "Joaquim", "Kalikey", "Luis"]
+# Define a largura padrão baseada no nome mais longo
+largura = max(len(nome) for nome in chamada) + 4
+
+# Linha superior
+linha_topo = "".join("+" + "-" * largura for _ in chamada) + "+"
+# Linha do meio com os nomes centralizados
+linha_meio = "".join("|" + nome.center(largura) for nome in chamada) + "|"
+# Linha inferior
+linha_base = "".join("+" + "-" * largura for _ in chamada) + "+"
+
+# Exibe o quadro
+print(linha_topo)
+print(linha_meio)
+print(linha_base)
+
+chamada = ["--Gustavo--", "Isabella", "Jamile","Joao",]
+# Define a largura padrão baseada no nome mais longo
+largura = max(len(nome) for nome in chamada) + 4
+
+# Linha superior
+linha_topo = "".join("+" + "-" * largura for _ in chamada) + "+"
+# Linha do meio com os nomes centralizados
+linha_meio = "".join("|" + nome.center(largura) for nome in chamada) + "|"
+# Linha inferior
+linha_base = "".join("+" + "-" * largura for _ in chamada) + "+"
+
+# Exibe o quadro
+print(linha_topo)
+print(linha_meio)
+print(linha_base)
+
+chamada = ["---Maria---", "Pedro", "Pedro", "Rafael",]
+# Define a largura padrão baseada no nome mais longo
+largura = max(len(nome) for nome in chamada) + 4
+
+# Linha superior
+linha_topo = "".join("+" + "-" * largura for _ in chamada) + "+"
+# Linha do meio com os nomes centralizados
+linha_meio = "".join("|" + nome.center(largura) for nome in chamada) + "|"
+# Linha inferior
+linha_base = "".join("+" + "-" * largura for _ in chamada) + "+"
+
+# Exibe o quadro
+print(linha_topo)
+print(linha_meio)
+print(linha_base)
+
+chamada = ["--Raphael--", "Roberto", "Sofia", "Carlos"]
+# Define a largura padrão baseada no nome mais longo
+largura = max(len(nome) for nome in chamada) + 4
+
+# Linha superior
+linha_topo = "".join("+" + "-" * largura for _ in chamada) + "+"
+# Linha do meio com os nomes centralizados
+linha_meio = "".join("|" + nome.center(largura) for nome in chamada) + "|"
+# Linha inferior
+linha_base = "".join("+" + "-" * largura for _ in chamada) + "+"
+
+# Exibe o quadro
+print(linha_topo)
+print(linha_meio)
+print(linha_base)
+
+chamada = ["-Stephanie-", "-----", "-----", "-----"]
+# Define a largura padrão baseada no nome mais longo
+largura = max(len(nome) for nome in chamada) + 4
+
+# Linha superior
+linha_topo = "".join("+" + "-" * largura for _ in chamada) + "+"
+# Linha do meio com os nomes centralizados
+linha_meio = "".join("|" + nome.center(largura) for nome in chamada) + "|"
+# Linha inferior
+linha_base = "".join("+" + "-" * largura for _ in chamada) + "+"
+
+# Exibe o quadro
+print(linha_topo)
+print(linha_meio)
+print(linha_base)
